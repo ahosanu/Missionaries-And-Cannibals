5ac470ed13004c18de2f605ddc1c45acec58e254
Ahosan Ullah <ahosanullah.en@gmail.com>
2018-03-13 21:49:11 +0600

    DSF Solve- Missionaries And Cannib

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         M_and_C init = new M_and_C(3, 3, 0, 0, "LEFT");
-        Node Goal = Node.BFS(init);
+        Node Goal = Node.DFS(init);
         System.out.println("Depth -> "+ Goal.Depth);
         Node.printSolution(Goal);
     }
--- src/Node.java
+++ src/Node.java
@@ -41,6 +41,39 @@ public class Node {
         return null;
     }
 
+    public  static Node DFS(M_and_C init){
+        Node root = new Node(init,null,0);
+        ArrayList<M_and_C> visited = new ArrayList<>();
+        Stack<Node> Fringe = new Stack<>();
+
+        Fringe.push(root);
+        while(!Fringe.isEmpty()){
+            Node n = Fringe.pop();
+            M_and_C Snode = n.states;
+            visited.add(Snode);
+            if(Snode.isGoalState()){
+                return n;
+            }
+            ArrayList<M_and_C>  Success = Snode.ApplyAction();
+            for(M_and_C s : Success){
+                boolean visit = true;
+                for(M_and_C v : visited) {
+                    if(s.ml == v.ml && s.cl == v.cl && s.mr == v.mr && s.cr == v.cr && s.b.equals(v.b)) {
+                        visit = false;
+                        break;
+                    }
+                }
+                if(visit) {
+                    Node node = new Node(s, n, n.Depth + 1);
+                    Fringe.push(node);
+                }
+            }
+        }
+        return null;
+    }
+
+
+
     public static  void printSolution(Node n){
         Stack<M_and_C> List = new Stack<>();
 
